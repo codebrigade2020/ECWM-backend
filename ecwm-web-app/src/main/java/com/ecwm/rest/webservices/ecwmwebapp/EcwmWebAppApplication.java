6d1bed5b8e07761b9d6325b1fec0a5bd10f70748
Deiva mani Kailasam <20294593@students.latrobe.edu.au>
2020-08-07 01:31:45 +1000

    Uploading All files

---
+++ ecwm-web-app/src/main/java/com/ecwm/rest/webservices/ecwmwebapp/EcwmWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.ecwm.rest.webservices.ecwmwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcwmWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcwmWebAppApplication.class, args);
+	}
+
+}
---
+++ ecwm-web-app/src/test/java/com/ecwm/rest/webservices/ecwmwebapp/EcwmWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecwm.rest.webservices.ecwmwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcwmWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
