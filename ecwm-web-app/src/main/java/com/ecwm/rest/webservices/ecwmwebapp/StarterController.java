2fae60a759612485d8c2648d5153d69a2344b885
Deiva mani Kailasam <20294593@students.latrobe.edu.au>
2020-08-07 11:13:13 +1000

    Uploading Setup file

--- ecwm-web-app/src/main/java/com/ecwm/rest/webservices/ecwmwebapp/StarterController.java
+++ ecwm-web-app/src/main/java/com/ecwm/rest/webservices/ecwmwebapp/StarterController.java
@@ -1,5 +1,6 @@
 package com.ecwm.rest.webservices.ecwmwebapp;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +8,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StarterController {
 	
+	//Testing Method 1
 	@RequestMapping(method=RequestMethod.GET, path= "/home")
 	public String starterMessage() {
 		return "App is up and running";
 	}
+	
+	//Testing Method 2
+	@RequestMapping(method=RequestMethod.GET, path= "/login/{username}/{password}")
+	public String login(@PathVariable String username,@PathVariable String password) {
+		
+		if(username.equals("deiva") && password.equals("mani"))
+		return "Login Success";
+		else
+		return "Login Failure";
+	}
+	
 
 }
