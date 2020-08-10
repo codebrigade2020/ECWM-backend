0402f1f36eaf6ffff343c722183dc3a7a70750e7
Deiva mani Kailasam <20294593@students.latrobe.edu.au>
2020-08-10 14:22:39 +1000

    Security Ctrl Changes - deiva

--- ecwm-web-app/src/main/java/com/codebrigade/ecwm/controller/SecurityController.java
+++ ecwm-web-app/src/main/java/com/codebrigade/ecwm/controller/SecurityController.java
@@ -1,20 +1,18 @@
 package com.codebrigade.ecwm.controller;
 
+import java.security.Principal;
+
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.ui.Model;
 
 @RestController
 public class SecurityController {
-		@Autowired
+	
 	   @PreAuthorize("hasRole('group1')")
 	   @RequestMapping("/")
-	   public String helloWorld() {
-		   return "Hello world";
+	   public String login(Principal principal) {
+		   return principal.getName()+ " logged in";
 	   }
 	   
 }
